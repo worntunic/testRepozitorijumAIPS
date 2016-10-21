0e59849c533cd7540766d01cec9a9a89079a2d7e
student <student@521-7>
2016-10-21 13:16:49 +0200

    Init.

---
+++ src/aipstest1/AIPSTest1.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aipstest1;
+
+/**
+ *
+ * @author student
+ */
+public class AIPSTest1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a = 1;
+    }
+}
